b73f92fe11ff99e48ba2b3d6e5e98f84c122e9ca
Jose Alejandro Cantoral Ramirez <jose.cantoral9535@alumnos.udg.mx>
2026-06-04 19:45:13 -0600

    Add files via upload

---
+++ Analizador de Texto/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Analizador_de_texto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el texto que deseas analizar");
+            string texto = Console.ReadLine();
+
+            int totalCaracteres = texto.Length;
+
+            int CaracteresSinEspacios = texto.Replace(" ", " ").Length;
+
+            string[] palabras = texto.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int totalpalabras = palabras.Length;
+
+            Console.WriteLine("\n Resultados analisis");
+            Console.WriteLine($"Caracteres totales: {totalCaracteres}");
+            Console.WriteLine($"Cacracteres sin espacios: {CaracteresSinEspacios }");
+            Console.WriteLine($"Total de palabras {totalpalabras}");
+
+            Console.WriteLine("Presione cualquier tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
