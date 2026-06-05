f9e5b82c7df4e144bcbfb2f2bbbd3a4f983e9d03
Jose Alejandro Cantoral Ramirez <jose.cantoral9535@alumnos.udg.mx>
2026-06-04 19:46:16 -0600

    Add files via upload

---
+++ Buscador y Reemplazador de Palabras/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ReemplazoTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Reemplazador de Texto ---");
+
+            Console.Write("Ingresa el texto completo: ");
+            string textoOriginal = Console.ReadLine();
+
+            Console.Write("Palabra a buscar: ");
+            string buscar = Console.ReadLine();
+
+            Console.Write("Palabra de reemplazo: ");
+            string reemplazo = Console.ReadLine();
+
+            // Realizamos el reemplazo
+            string textoResultante = textoOriginal.Replace(buscar, reemplazo);
+
+            Console.WriteLine("\n--- Resultado ---");
+            Console.WriteLine(textoResultante);
+
+            Console.ReadKey();
+        }
+    }
+}
