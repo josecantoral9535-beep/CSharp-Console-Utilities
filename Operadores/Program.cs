16ea612fd06bdd551b9ba438f780115e1c641027
Jose Alejandro Cantoral Ramirez <jose.cantoral9535@alumnos.udg.mx>
2026-06-04 19:50:34 -0600

    Add files via upload

---
+++ Operadores/Program.cs
@@ -0,0 +1,37 @@
+﻿    using System;
+
+    namespace OperadoresAsignacion
+    {
+
+        class Program
+        {
+
+            static void Main(String[] args)
+            {
+                Console.WriteLine("Ingresa un numero entero");
+                int numero = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"\n Numero inicial:{numero}");
+
+                numero += 10;
+                Console.WriteLine($"Despues de sumar 10 = {numero}");
+
+                numero -= 5;
+                Console.WriteLine($"Despues de restar 5 = {numero}");
+
+                numero *= 2;
+                Console.WriteLine($"Despues de multiplicar por 2 es = {numero}");
+
+                numero /= 3;
+                Console.WriteLine($"Despues de dividir por 3 es = {numero}");
+
+                numero %= 4;
+                Console.WriteLine($"Despues de obtener el modulo 4 = {numero}");
+
+
+                Console.WriteLine("Presione cualquier tecla para salir");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
