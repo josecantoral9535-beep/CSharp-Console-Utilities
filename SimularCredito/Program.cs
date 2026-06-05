cb88ab63a9405bfba3ee3e103781ec2cbe5d9643
Jose Alejandro Cantoral Ramirez <jose.cantoral9535@alumnos.udg.mx>
2026-06-04 19:51:13 -0600

    Add files via upload

---
+++ SimularCredito/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EvaluacionCredito
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese su salario");
+            double salario = double.Parse(Console.ReadLine());
+
+            if (edad >= 18 && salario >= 2000)
+            {
+
+                Console.WriteLine("Tu credito ha sido aprobado");
+
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("Lo sentimos, no cumples los requisitos, tu credito no fue aprobado");
+
+            }
+
+            Console.WriteLine("\n Presione cualquier tecla para salir");
+            Console.ReadKey();
+
+        }
+
+    }
+}
